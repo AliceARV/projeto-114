f83e06bbe92273c5b2956fd603b68a356e365550
AliceARV <104326827+AliceARV@users.noreply.github.com>
2023-06-30 16:36:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clownNose = loadImage('https://img.icons8.com/?size=512&id=RFY5zRovZ0jB&format=png');
+
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet foi inicializado');
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    noseX = results[0].pose.nose.x  ;
+    noseY = results[0].pose.nose.y ;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  //image(clownNose, noseX, noseY, 30, 30);
+  //image(boca, bocaX, bocaY, 30, 30);
+  //image(boca, jajaX, jajaY, 30, 30);
+  fill(255, 51, 0);
+  stroke(204, 0, 0);
+  circle(noseX, noseY, 20);
+}
+
+  function takeSnapshot() {
+    save('myFilterImage.png');
+  }
